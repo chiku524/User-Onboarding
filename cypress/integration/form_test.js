7e8316e4c45368d49546c97fc8a0b1f9c855a2c5
Nico Chikuji <chiku524@icloud.com>
2020-06-14 20:21:06 -0400

    made the tests for checking if input is empty

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -8,6 +8,9 @@ describe('Cypress testing for the form inputs', function(){
         cy.get('[data-cy="password"]').type("qwerty123456").should("have.value", "qwerty123456");
         cy.get('[data-cy="terms_of_service"]').check().should("be.checked");
         cy.get('[data-cy="role"]').select('iPhone Developer')
+        cy.get('[data-cy="submit"').should("be.enabled");
+        cy.get('[data-cy="name"]').invoke('val').should("not.be.empty");
+        cy.get('[data-cy="password"]').invoke('val').should("have.length.greaterThan", 7);
         cy.get('[data-cy="submit"').click();
     })
 })
